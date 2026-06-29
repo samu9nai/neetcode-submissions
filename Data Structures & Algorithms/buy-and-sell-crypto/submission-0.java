f4b83664a48777841a9ada1de0b52c6ad476773a
Mingyu Joung <samu9nai@gmail.com>
2026-06-29 17:40:42 +0900

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+        int maxProf = 0;
+
+        while (right < prices.length) {
+            if (prices[left] < prices[right]) {
+                int profit = prices[right] - prices[left];
+                maxProf = Math.max(maxProf, profit);
+            } else left = right;
+
+            right++;
+        }
+        return maxProf;
+    }
+}
